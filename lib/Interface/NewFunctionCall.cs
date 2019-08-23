2a4f96fe12ed85b5437bd4cebd00e4aa5627d4db
Matheus Lessa <matheus@fastmail.com>
2019-08-23 08:48:54 -0300

    tentando nova parada

---
+++ lib/Interface/NewFunctionCall.cs
@@ -0,0 +1,30 @@
+public struct NewFunctionCall<A, R>
+	where A : ITuple
+	where R : IMarshalable
+{
+	private readonly int codeIndex;
+	private readonly ushort functionIndex;
+
+	public NewFunctionCall(int codeIndex, ushort functionIndex)
+	{
+		this.codeIndex = codeIndex;
+		this.functionIndex = functionIndex;
+	}
+
+	public R Call<M>(ref M marshaler, VirtualMachine vm, A arguments) where M : IMarshaler
+	{
+		vm.valueStack.PushBack(new ValueData(functionIndex));
+		vm.callframeStack.PushBack(new CallFrame(
+			-1,
+			vm.chunk.bytes.count - 1,
+			vm.valueStack.count
+		));
+		vm.callframeStack.PushBack(new CallFrame(
+			functionIndex,
+			codeIndex,
+			vm.valueStack.count
+		));
+
+		return default;
+	}
+}
